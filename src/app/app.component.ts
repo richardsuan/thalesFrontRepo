dff58b69ee25cf7a6a7896b5c0818ce786970aac
rsuan-lab <47155949+richardsuan@users.noreply.github.com>
2023-06-01 01:12:34 -0500

    add style from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {ServiceComponent} from './employee-service/service.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(public serviceComponent : ServiceComponent) {
